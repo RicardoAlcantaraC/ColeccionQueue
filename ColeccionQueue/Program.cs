cd6c9d0cf26d44ba26085a906ed898add1c617af
ricardo-alc <ralcantarac568@alumno.uaemex.mx>
2021-02-02 15:18:37 -0600

    Agregar archivos de proyecto.

---
+++ ColeccionQueue/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ColeccionQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ejemplo1();              
+        }
+
+        public static void ejemplo1()
+        {
+            Queue<int> numeros = new Queue<int>();
+
+            //Agregar elementos
+            foreach (int item in new int[5] {1,2,3,4,5})
+            {
+                numeros.Enqueue(item);
+            }
+
+            Console.WriteLine("Cola original");
+            imprimir(numeros);
+
+            //Eliminando elementos
+            numeros.Dequeue();
+            Console.WriteLine("Cola después de eliminar elementos");
+            imprimir(numeros);
+        }
+
+        public static void imprimir(Queue<int> cola)
+        {
+            foreach (int item in cola)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+    }
+}
